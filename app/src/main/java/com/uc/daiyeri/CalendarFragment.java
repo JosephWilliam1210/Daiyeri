3cc17b775f699436431d208e3a0f982113df593f
JosephWilliam1210 <62556984+JosephWilliam1210@users.noreply.github.com>
2020-05-05 02:37:56 -0700

    Update CalendarFragment.java

--- app/src/main/java/com/uc/daiyeri/CalendarFragment.java
+++ app/src/main/java/com/uc/daiyeri/CalendarFragment.java
@@ -13,6 +13,7 @@ public class CalendarFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_calendar,container,false);
+        View view = inflater.inflate(R.layout.fragment_calendar, container, false);
+        return view;
     }
 }
