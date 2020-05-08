e8587e23b49c13abfd0c1e1d1d0610d8e5f4485f
JosephWilliam1210 <62556984+JosephWilliam1210@users.noreply.github.com>
2020-05-08 08:12:08 -0700

    Update User.java

--- app/src/main/java/com/uc/daiyeri/User.java
+++ app/src/main/java/com/uc/daiyeri/User.java
@@ -1,11 +1,16 @@
 package com.uc.daiyeri;
 
-public class User {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class User implements Parcelable {
     private String title, note;
+    int id;
 
     public User(String title, String note) {
         this.title = title;
         this.note = note;
+        this.id = id;
     }
 
     public String getTitle() {
@@ -23,4 +28,43 @@ public class User {
     public void setNote(String note) {
         this.note = note;
     }
+
+    public int getId(){
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.title);
+        dest.writeString(this.note);
+        dest.writeInt(this.id);
+    }
+
+    protected User(Parcel in) {
+        this.title = in.readString();
+        this.note = in.readString();
+        this.id = in.readInt();
+    }
+
+    public static final Parcelable.Creator<User> CREATOR = new Parcelable.Creator<User>() {
+        @Override
+        public User createFromParcel(Parcel source) {
+            return new User(source);
+        }
+
+        @Override
+        public User[] newArray(int size) {
+            return new User[size];
+        }
+    };
 }
